9dadf6c6e3cb7e2961dd6ed53c859ccfb3280106
1039168735@qq.com <1039168735@qq.com>
2017-09-19 23:03:21 +0800

    icon button

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,8 @@
       </el-row>
       <el-row>
           <el-col :span="24">
-              <div class="grid-content bg-purple-dark"></div>
+              <div class="grid-content bg-purple-dark">
+              </div>
           </el-col>
       </el-row>
       <el-row>
@@ -48,6 +49,45 @@
           <el-col :span="24">
               <div class="grid-content bg-purple-dark danger">
                   Icon图标
+              </div>
+          </el-col>
+      </el-row>
+      <i class="el-icon-edit"></i>
+      <i class="el-icon-check"></i>
+      <i class="el-icon-more"></i>
+      <i class="el-icon-setting"></i>
+      <i class="el-icon-time"></i>
+      <i class="el-icon-loading"></i>
+      <br>
+      <br>
+      <el-row>
+          <el-col :span="24">
+              <div class="danger grid-content bg-purple-light">
+                  Button按钮
+              </div>
+          </el-col>
+      </el-row>
+      <el-button :disabled="true">默认按钮</el-button>
+      <el-button type="text">文字按钮</el-button>
+      <el-button type="danger" >危险按钮</el-button>
+      <br>
+      <br>
+      <el-button type="warning" icon="edit"></el-button>
+      <el-button type="primary" icon="setting">设置按钮</el-button>
+      <el-button type="danger" :loading="true">加载中</el-button>
+      <br>
+      <br>
+      <el-button type="primary" icon="setting" size="small">设置按钮</el-button>
+      <el-button-group>
+          <el-button type="primary" icon="arrow-left">上一页</el-button>
+          <el-button type="primary">下一页<i class="el-icon-arrow-right el-icon--right"></i></el-button>
+      </el-button-group>
+      <br>
+      <br>
+      <el-row>
+          <el-col>
+              <div class="danger grid-content bg-purple">
+                  单选按钮
               </div>
           </el-col>
       </el-row>
